8c16b44550365f66e3ed58868ab85ea52c59cdf5
Dizzard <marconacken@web.de>
2019-11-30 13:55:44 +0100

    js aufgeteilt

---
+++ HTML/firebase.js
@@ -0,0 +1,39 @@
+//Firebase Zeugs
+/* const firebase = require("firebase");
+
+// Required for side-effects
+require("firebase/firestore"); */
+
+// Initialize Cloud Firestore through Firebase
+var config = {
+  "apiKey": "AIzaSyAUHFqNblYKWTCrje07bkdNeorlQ4IuNEs",
+  "appId": "1:728300584033:web:ed2bf8534da27bacc53e20",
+  "authDomain": "iamphysiotherappy-17757.firebaseapp.com",
+  "databaseURL": "https://iamphysiotherappy-17757.firebaseio.com",
+  "measurementId": "G-SR5XN6N10J",
+  "messagingSenderId": "728300584033",
+  "projectId": "iamphysiotherappy-17757",
+  "storageBucket": "iamphysiotherappy-17757.appspot.com"
+};
+firebase.initializeApp(config);
+
+var db = firebase.firestore();
+
+const inputTextField = document.querySelector("testtext");
+const saveButton = document.querySelector("saveButton");
+
+/* db.collection("Nutzer").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
+}); */
+
+saveButton.addEventListener("click", function() {
+	const textToSave = inputTextField.value;
+	console.log("Folgender Text wird gespeichert " + textToSave);
+	db.collection("Nutzer").get().then((querySnapshot) => {
+		querySnapshot.forEach((doc) => {
+			console.log(`${doc.id} => ${doc.data()}`);
+		});
+	});
+});
